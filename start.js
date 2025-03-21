cf13b44893bd9cc4607063a2aece7b1baf089562
boramalaj <109850852+BoraMalaj@users.noreply.github.com>
2025-03-21 10:06:12 +0000

    changed start script

--- start.js
+++ start.js
@@ -3,6 +3,6 @@
 // Redirect to index.html after 5 seconds, if user doesn't click 'Continue to the Game'
 document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
-        window.location.href = 'index.html';
+        window.location.href = 'game.html';
     }, 5000); // Redirect after 5 seconds
 });
